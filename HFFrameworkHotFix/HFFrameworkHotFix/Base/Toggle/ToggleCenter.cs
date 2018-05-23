d7e8b6621137f0c1a4225431376fb4f8aa444ae6
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-05-23 14:31:30 +0800

    修改了一些东西

---
+++ HFFrameworkHotFix/HFFrameworkHotFix/Base/Toggle/ToggleCenter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace HotFix
+{
+    public class ToggleCenter : BaseElement
+    {
+        private int currentIndex = -1;
+
+        private List<ToggleItem> list = new List<ToggleItem>();
+
+        private Action<int> callback;
+
+        public void AddListener(Action<int> callback)
+        {
+            this.callback = callback;
+        }
+
+        public void AddToggleItem(ToggleItem it)
+        {
+            it.center = this;
+            list.Add(it);
+        }
+
+        public void ItemCallback(int index)
+        {
+            currentIndex = index;
+            for (int i = 0; i < list.Count; i++)
+            {
+                ToggleItem item = list[i];
+                if (currentIndex == item.index)
+                {
+                    item.StartSelect(true);
+                    callback(currentIndex);
+                }
+                else
+                {
+                    item.StartSelect(false);
+                }
+            }
+        }
+
+        public void SetSelect(int index)
+        {
+            ItemCallback(index);
+        }
+
+    }
+}
+

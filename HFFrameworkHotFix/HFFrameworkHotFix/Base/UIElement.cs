bcbceec83968208cd2c504fff25dc1a33b98c360
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-05-25 15:12:41 +0800

    修改了一些东西

--- HFFrameworkHotFix/HFFrameworkHotFix/Base/UIElement.cs
+++ HFFrameworkHotFix/HFFrameworkHotFix/Base/UIElement.cs
@@ -5,32 +5,68 @@ using System;
 
 namespace HotFix
 {
+    public enum UIHideType
+    {
+        Scale,
+        CanvasGroup
+    }
+
     public class UIElement : BaseElement
     {
-        public const float FadeTime = 0.25f;
+        /// <summary>
+        ///  UI 隐藏 类型
+        /// </summary>
+        private UIHideType hideType = UIHideType.Scale;
 
-        public static Vector3 ShowVector3 = Vector3.one;
-        public static Vector3 HideVector3 = Vector3.zero;
+        private CanvasGroup canvasGroup;
+        /// <summary>
+        ///  如果使用了 CanvasGroup
+        /// </summary>
+        public CanvasGroup CanvasGroup
+        {
+            get
+            {
+                if (canvasGroup==null&&gameObject!=null)
+                {
+                    canvasGroup = gameObject.AddComponent<CanvasGroup>();
+                }
+                return canvasGroup;
+            }
+        }
 
         private bool isShow;
+        /// <summary>
+        ///  对于UI 的显示 或者隐藏 如果使用 SetActive 那么会产生额外的GC  所以使用缩放 或者 CanvasGroup.alpha 方法 降低DrawCall 和GC 
+        /// </summary>
         public virtual bool IsShow
         {
             set
             {
                 isShow = value;
-                if (isShow == true)
+                if (gameObject != null)
                 {
-                    if (gameObject != null && transform.localScale != UIElement.ShowVector3)
+                    if (isShow == true)
                     {
-                        transform.localScale = UIElement.ShowVector3;
+                        if (hideType == UIHideType.Scale)
+                        {
+                            transform.localScale = Vector3.one;
+                        }
+                        else
+                        {
+                            CanvasGroup.alpha = 1;
+                        }
                         ElementDidAppear();
                     }
-                }
-                else
-                {
-                    if (gameObject != null && transform.localScale != UIElement.HideVector3)
+                    else
                     {
-                        transform.localScale = UIElement.HideVector3;
+                        if (hideType == UIHideType.Scale)
+                        {
+                            transform.localScale = Vector3.zero;
+                        }
+                        else
+                        {
+                            CanvasGroup.alpha = 0;
+                        }
                         ElementDidDisAppear();
                     }
                 }

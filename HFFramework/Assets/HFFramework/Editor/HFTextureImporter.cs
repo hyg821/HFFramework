6cf08e68499a2b5dc8bfab6bcc84f8f26e057acf
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-05-25 15:00:28 +0800

    修改了一些东西

---
+++ HFFramework/Assets/HFFramework/Editor/HFTextureImporter.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+/// <summary>
+///  不能使用命名空间  如果使用系统无法反射到导入对象
+/// </summary>
+public class HFTextureImporter : AssetPostprocessor
+{
+    public static string TextureExtension = "_dither565";
+
+    /// <summary>
+    ///  图片导入之前的设置
+    /// </summary>
+    void OnPreprocessTexture()
+    {
+    }
+
+    /// <summary>
+    ///  图片导入时候的设置
+    /// </summary>
+    /// <param name="texture"></param>
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        Dither4444(texture);
+    }
+
+    public void Dither4444(Texture2D texture)
+    {
+        if (assetPath.Contains(TextureExtension))
+        {
+
+            var texw = texture.width;
+            var texh = texture.height;
+
+            var pixels = texture.GetPixels();
+            var offs = 0;
+
+            var k1Per31 = 1.0f / 31.0f;
+
+            var k1Per32 = 1.0f / 32.0f;
+            var k5Per32 = 5.0f / 32.0f;
+            var k11Per32 = 11.0f / 32.0f;
+            var k15Per32 = 15.0f / 32.0f;
+
+            var k1Per63 = 1.0f / 63.0f;
+
+            var k3Per64 = 3.0f / 64.0f;
+            var k11Per64 = 11.0f / 64.0f;
+            var k21Per64 = 21.0f / 64.0f;
+            var k29Per64 = 29.0f / 64.0f;
+
+            var k_r = 32; //R&B压缩到5位，所以取2的5次方
+            var k_g = 64; //G压缩到6位，所以取2的6次方
+
+            for (var y = 0; y < texh; y++)
+            {
+                for (var x = 0; x < texw; x++)
+                {
+                    float r = pixels[offs].r;
+                    float g = pixels[offs].g;
+                    float b = pixels[offs].b;
+
+                    var r2 = Mathf.Clamp01(Mathf.Floor(r * k_r) * k1Per31);
+                    var g2 = Mathf.Clamp01(Mathf.Floor(g * k_g) * k1Per63);
+                    var b2 = Mathf.Clamp01(Mathf.Floor(b * k_r) * k1Per31);
+
+                    var re = r - r2;
+                    var ge = g - g2;
+                    var be = b - b2;
+
+                    var n1 = offs + 1;
+                    var n2 = offs + texw - 1;
+                    var n3 = offs + texw;
+                    var n4 = offs + texw + 1;
+
+                    if (x < texw - 1)
+                    {
+                        pixels[n1].r += re * k15Per32;
+                        pixels[n1].g += ge * k29Per64;
+                        pixels[n1].b += be * k15Per32;
+                    }
+
+                    if (y < texh - 1)
+                    {
+                        pixels[n3].r += re * k11Per32;
+                        pixels[n3].g += ge * k21Per64;
+                        pixels[n3].b += be * k11Per32;
+
+                        if (x > 0)
+                        {
+                            pixels[n2].r += re * k5Per32;
+                            pixels[n2].g += ge * k11Per64;
+                            pixels[n2].b += be * k5Per32;
+                        }
+
+                        if (x < texw - 1)
+                        {
+                            pixels[n4].r += re * k1Per32;
+                            pixels[n4].g += ge * k3Per64;
+                            pixels[n4].b += be * k1Per32;
+                        }
+                    }
+
+                    pixels[offs].r = r2;
+                    pixels[offs].g = g2;
+                    pixels[offs].b = b2;
+
+                    offs++;
+                }
+            }
+
+            texture.SetPixels(pixels);
+            EditorUtility.CompressTexture(texture, TextureFormat.RGB565, TextureCompressionQuality.Best);
+
+        }
+    }
+
+}

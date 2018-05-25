e1b536672c4e8f2cc9eeaba71462fd2e5eff7586
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-05-25 13:48:30 +0800

    修改了一些东西

--- HFFrameworkHotFix/HFFrameworkHotFix/Base/UIElement.cs
+++ HFFrameworkHotFix/HFFrameworkHotFix/Base/UIElement.cs
@@ -7,11 +7,39 @@ namespace HotFix
 {
     public class UIElement : BaseElement
     {
-        public static Vector3 showVector = new Vector3(1, 1, 1);
-
-        public static Vector3 hideVector = Vector3.zero;
-
         public const float FadeTime = 0.25f;
+
+        public static Vector3 ShowVector3 = Vector3.one;
+        public static Vector3 HideVector3 = Vector3.zero;
+
+        private bool isShow;
+        public virtual bool IsShow
+        {
+            set
+            {
+                isShow = value;
+                if (isShow == true)
+                {
+                    if (gameObject != null && transform.localScale != UIElement.ShowVector3)
+                    {
+                        transform.localScale = UIElement.ShowVector3;
+                        ElementDidAppear();
+                    }
+                }
+                else
+                {
+                    if (gameObject != null && transform.localScale != UIElement.HideVector3)
+                    {
+                        transform.localScale = UIElement.HideVector3;
+                        ElementDidDisAppear();
+                    }
+                }
+            }
+            get
+            {
+                return IsShow;
+            }
+        }
     }
 }
 

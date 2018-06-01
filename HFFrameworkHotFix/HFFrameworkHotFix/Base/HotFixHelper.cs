7c8decfb70457fd91eba8010af52abb78d2f28ec
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-06-01 18:30:18 +0800

    修改了一些东西

---
+++ HFFrameworkHotFix/HFFrameworkHotFix/Base/HotFixHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HFFramework;
+
+namespace HotFix
+{
+    public class HotFixHelper : BaseElement
+    {
+        public static object ReflectCreate(string nameSpace,string className)
+        {
+            Type t = Type.GetType(nameSpace+ "." + className);
+            object obj = Activator.CreateInstance(t);
+            return obj;
+        }
+    }
+}

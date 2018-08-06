939011bcd8c7b552c5ebf58bdcf31d5b824966bd
pooqqqqq@aliyun.com <pooqqqqq@aliyun.com>
2018-08-06 11:08:42 +0800

    修改了一些东西

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+根目录三个文件夹 分别是 
+1.Unity主工程  
+2.热更工程 
+3.工具&&DLL 
+如果想使用 dll 热更protobuf  需要删除工程内 Google.Protobuf.dll 
+并且按照https://github.com/gongxun/protobuf3-for-Unity-and-ILRuntime 去重新导入并且设置protobuf 
--- Tools/Build_bat/buildandroid.bat
+++
@@ -1,8 +0,0 @@
-@echo off  
-@set unity="E:\Unity2017\Editor\Unity.exe"  
-echo 正在更新版本
-echo 正在生成android客户端...  
-%unity%  -batchmode -quit -nographics -executeMethod BatchMode.BuildAndroid
-echo android客户端生成完毕!  
-explorer.exe c:\android
-pause  

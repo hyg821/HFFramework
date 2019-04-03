8bc39167fa7d7b8fd3eddbc4b51a7e80798aa313
魔王 <934533623@qq.com>
2019-04-04 01:37:27 +0800

    1

--- HFFramework/Assets/Core/Scripts/Functions/LearnMatrix.cs
+++ HFFramework/Assets/Core/Scripts/Functions/LearnMatrix.cs
@@ -6,10 +6,26 @@ namespace HFFramework
 {
     public class LearnMatrix : MonoBehaviour
     {
+        public Matrix4x4 localToWorldMatrix;
+        public Matrix4x4 worldToLocalMatrix;
+
+        public Vector4 worldV;
+        public Vector4 localV;
+
         void Start()
         {
-            print(gameObject.transform.localToWorldMatrix);
+
         }
+
+        private void Update()
+        {
+            localToWorldMatrix = transform.localToWorldMatrix;
+            worldToLocalMatrix = transform.worldToLocalMatrix;
+
+            worldV = localToWorldMatrix * new Vector4(10, 5, 0, 1);
+            localV = worldToLocalMatrix * worldV;
+        }
+
     }
 }
 

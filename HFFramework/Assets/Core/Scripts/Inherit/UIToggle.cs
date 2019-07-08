2af23010179ed6f98336abc150ab4f798e31115c
houyingge <houyingge@mttop.cn>
2019-07-08 12:23:26 +0800

    修改

---
+++ HFFramework/Assets/Core/Scripts/Inherit/UIToggle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace ETModel
+{
+    public class UIToggle : MonoBehaviour
+    {
+        public  UIToggleGroup group;
+
+        public int index;
+
+        private int selectCode = -1;
+
+        public Action<bool> OnValueChange;
+
+        private void Awake()
+        {
+            
+        }
+
+        public void Init(UIToggleGroup group,int index)
+        {
+            this.group = group;
+            this.index = index;
+        }
+
+        public void m_Select(int selectCode)
+        {
+            if (OnValueChange!=null&&this.selectCode!=selectCode)
+            {
+                this.selectCode = selectCode;
+                bool b = selectCode == 1 ? true : false;
+                OnValueChange(b);
+            }
+        }
+
+        public void Select()
+        {
+            group.Select(index);
+        }
+
+    }
+}
+

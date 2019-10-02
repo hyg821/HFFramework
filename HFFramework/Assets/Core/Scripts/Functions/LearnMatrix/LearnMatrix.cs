3928f9e5bde5e10b5f9aebe1d81775c246bba0d2
hyg <934533623@qq.com>
2019-10-02 11:50:42 +0800

    修改bug

--- HFFramework/Assets/Core/Scripts/Functions/LearnMatrix/LearnMatrix.cs
+++ HFFramework/Assets/Core/Scripts/Functions/LearnMatrix/LearnMatrix.cs
@@ -2,30 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace HFFramework
+public class LearnMatrix : MonoBehaviour
 {
-    public class LearnMatrix : MonoBehaviour
-    {
-        public Matrix4x4 localToWorldMatrix;
-        public Matrix4x4 worldToLocalMatrix;
+    public Transform point;
 
-        public Vector4 worldV;
-        public Vector4 localV;
+    [Space]
 
-        void Start()
-        {
+    public Vector4 l2w0;
+	public Vector4 l2w1;
+	public Vector4 l2w2;
+	public Vector4 l2w3;
+	
+	[Space]
+	
+	public Vector4 w2l0;
+	public Vector4 w2l1;
+	public Vector4 w2l2;
+	public Vector4 w2l3;
 
-        }
+	[Space]
+	
+	public Vector3 pointLoacalPosition;
+	public Vector3 pointPosition;
 
-        private void Update()
-        {
-            localToWorldMatrix = transform.localToWorldMatrix;
-            worldToLocalMatrix = transform.worldToLocalMatrix;
+	[Space]
+	public Vector3 New_pointLoacalPosition;
+	public Vector3 New_pointPosition;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		l2w0 = transform.localToWorldMatrix.GetRow(0);
+		l2w1 = transform.localToWorldMatrix.GetRow(1);
+		l2w2 = transform.localToWorldMatrix.GetRow(2);
+		l2w3 = transform.localToWorldMatrix.GetRow(3);
+		
+		w2l0 = transform.worldToLocalMatrix.GetRow(0);
+		w2l1 = transform.worldToLocalMatrix.GetRow(1);
+		w2l2 = transform.worldToLocalMatrix.GetRow(2);
+		w2l3 = transform.worldToLocalMatrix.GetRow(3);
 
-            worldV = localToWorldMatrix * new Vector4(10, 5, 0, 1);
-            localV = worldToLocalMatrix * worldV;
-        }
+		pointLoacalPosition = point.localPosition;
+		pointPosition = point.position;
 
-    }
+		New_pointLoacalPosition = transform.worldToLocalMatrix * ToVector4(point.position);
+		New_pointPosition = transform.localToWorldMatrix *ToVector4(point.localPosition);
+	}
+
+	public Vector4 ToVector4(Vector3 vv)
+	{
+		return  new  Vector4(vv.x,vv.y,vv.z,1);
+	}
+	
 }
 

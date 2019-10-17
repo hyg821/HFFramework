3509db3421f89ffe95a92eb0a958e4ab0b498e39
hyg <934533623@qq.com>
2019-10-17 11:59:01 +0800

    修改bug

---
+++ HFFramework/Assets/Core/Scripts/Functions/BoxCollider/ViewConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewConfig : MonoBehaviour
+{
+    public Transform startPosition;
+    public float xRoation;
+    public Box box;
+    private void Awake()
+    {
+        box = GetComponent<Box>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (startPosition!=null)
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawCube(startPosition.transform.position+new Vector3(0,10,0), Vector3.one);
+        }
+    }
+
+}

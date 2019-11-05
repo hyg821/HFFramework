e05a437767a31cdbaee17d0b75ea60fce48570bb
hyg <934533623@qq.com>
2019-11-05 18:01:46 +0800

    修改bug

--- HFFramework/Assets/Core/Scripts/Functions/ClickEventForward.cs
+++ HFFramework/Assets/Core/Scripts/Functions/ClickEventForward.cs
@@ -10,26 +10,58 @@ namespace HFFramework
     ///  这个类 的主要作用是 点击事件转发 可以做点击事件渗透 比如一个按钮挡住了别的按钮
     ///   这个类变换之后 就可以实现 所有的事件转发 比如 scrollview 滑动嵌套 的转发 
     /// </summary>
-    public class ClickEventForward : MonoBehaviour,IPointerClickHandler, ISubmitHandler, IEventSystemHandler
+    public class ClickEventForward : MonoBehaviour, IMoveHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler, IEventSystemHandler, IPointerClickHandler, ISubmitHandler
     {
         public GameObject forwardTarget;
 
-        //监听点击
+        public void OnDeselect(BaseEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.deselectHandler);
+        }
+
+        public void OnMove(AxisEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.moveHandler);
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
             PassEvent(eventData, ExecuteEvents.pointerClickHandler);
         }
 
-        public void OnSubmit(BaseEventData eventData)
+        public void OnPointerDown(PointerEventData eventData)
         {
-            PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+            PassEvent(eventData, ExecuteEvents.pointerDownHandler);
         }
 
-        //把事件透下去
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerEnterHandler);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerExitHandler);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerUpHandler);
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.selectHandler);
+        }
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.submitHandler);
+        }
+
         public void PassEvent<T>(BaseEventData data, ExecuteEvents.EventFunction<T> function)where T : IEventSystemHandler
         {
             ExecuteEvents.Execute(forwardTarget, data, function);
         }
     }
-
 }

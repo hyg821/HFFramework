8366d7be97550ab95415e401aa019099098d9466
魔王 <934533623@qq.com>
2020-01-04 11:57:31 +0800

    1

---
+++ HFFramework/Assets/Core/Scripts/Base/Singleton.cs
@@ -0,0 +1,30 @@
+﻿namespace HFFramework
+{
+    public class Singleton<T> where T : Entity, new()
+    {
+        /// <summary>
+        ///  私有
+        /// </summary>
+        private static T instance;
+
+        /// <summary>
+        ///  单例
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                    instance.Awake();
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+    }
+}

b9c9905d0a7366db2ecbaefc42b77c835c80a536
hyg821 <934533623@qq.com>
2020-08-29 19:29:01 +0800

    修改bug

---
+++ HFFramework/Assets/Core/Editor/AssetConfigEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEditor.SceneManagement;
+
+namespace HFFramework.Editor
+{
+    [CustomEditor(typeof(AssetConfig))]
+    public class AssetConfigEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            AssetConfig info = target as AssetConfig;
+
+            if (GUILayout.Button("设置文件夹AssetBundleName"))
+            {
+                string path = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/Assets")) +"/"+ AssetDatabase.GetAssetPath(info);
+                path = path.Substring(0, path.LastIndexOf("/"));
+                //Debug.LogError("path  " + path);
+                AssetBundleTools.SetAssetConfig(path, true, false);
+            }
+        }
+    }
+}
+

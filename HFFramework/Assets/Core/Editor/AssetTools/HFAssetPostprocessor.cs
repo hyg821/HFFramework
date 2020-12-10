a94110b93c5c660dc13cac1568515f4683160270
hyg821 <934533623@qq.com>
2020-12-10 13:44:09 +0800

    修改bug

--- HFFramework/Assets/Core/Editor/AssetTools/HFAssetPostprocessor.cs
+++ HFFramework/Assets/Core/Editor/AssetTools/HFAssetPostprocessor.cs
@@ -5,6 +5,8 @@ using UnityEditor;
 using HFFramework;
 using HFFramework.Editor;
 using LitJson;
+using System.IO;
+using UnityEditor.Presets;
 
 namespace UnityEditor
 {
@@ -152,7 +154,32 @@ namespace UnityEditor
         /// </summary>
         void OnPreprocessAsset()
         {
+            // 确保我们在第一次导入资源时应用预设。
+            if (assetImporter.importSettingsMissing)
+            {
+                // 获取当前导入的资源文件夹。
+                var path = Path.GetDirectoryName(assetPath);
+                while (!string.IsNullOrEmpty(path))
+                {
+                    // 查找此文件夹中的所有预设资源。
+                    var presetGuids = AssetDatabase.FindAssets("t:Preset", new[] { path });
+                    foreach (var presetGuid in presetGuids)
+                    {
+                        // 确保不是在子文件夹中测试预设。
+                        string presetPath = AssetDatabase.GUIDToAssetPath(presetGuid);
+                        if (Path.GetDirectoryName(presetPath) == path)
+                        {
+                            //加载预设，然后尝试将其应用于导入器。
+                            var preset = AssetDatabase.LoadAssetAtPath<Preset>(presetPath);
+                            if (preset.ApplyTo(assetImporter))
+                                return;
+                        }
+                    }
 
+                    //在父文件夹中重试。
+                    path = Path.GetDirectoryName(path);
+                }
+            }
         }
 
         /// <summary>

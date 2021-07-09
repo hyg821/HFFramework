f4dd1473d6b3556e3de680a0da173fbd84339545
hyg821 <934533623@qq.com>
2021-07-09 18:16:22 +0800

    1

---
+++ HFFramework/Assets/Core/Editor/AssetBundleTools/AssetBundleWindow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace HFFramework.Editor
+{
+    public class AssetConfigInfo
+    {
+        public string path;
+        public AssetConfig config;
+
+        public AssetConfigInfo(string path, AssetConfig config)
+        {
+            this.path = path;
+            this.config = config;
+        }
+    }
+
+    public class AssetBundleWindow : EditorWindow
+    {
+        [MenuItem("资源/总览窗口",false,0)]
+        public static void ShowWindow()
+        {
+            AssetBundleWindow window = GetWindow<AssetBundleWindow>();
+            window.Show();
+        }
+
+        public List<AssetConfigInfo> configList = new List<AssetConfigInfo>();
+
+        private void OnEnable()
+        {
+            configList.Clear();
+            string[] guids = AssetDatabase.FindAssets("t:AssetConfig");
+            for (int i = 0; i < guids.Length; i++)
+            {
+                string guid = guids[i];
+                string configPath = AssetDatabase.GUIDToAssetPath(guid);
+                AssetConfig assetConfig = AssetDatabase.LoadAssetAtPath(configPath, typeof(AssetConfig)) as AssetConfig;
+                configList.Add(new AssetConfigInfo(configPath, assetConfig));
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+
+            for (int i = 0; i < configList.Count; i++)
+            {
+                AssetConfigInfo configInfo = configList[i];
+                EditorGUILayout.ObjectField(configInfo.config.assetbundleName, configInfo.config, typeof(AssetConfig),true);
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+    }
+}

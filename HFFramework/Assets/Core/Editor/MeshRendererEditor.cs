3692fb5972426f148bead5a8c90261ff2922d5cd
hyg821 <934533623@qq.com>
2021-08-31 21:01:54 +0800

    修改bug

---
+++ HFFramework/Assets/Core/Editor/MeshRendererEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace HFFramework.Editor
+{
+    [CustomEditor(typeof(MeshRenderer))]
+    public class MeshRendererEditor : UnityEditor.Editor
+    {
+        MeshRenderer meshRenderer;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            meshRenderer = target as MeshRenderer;
+
+            string[] layerNames = new string[SortingLayer.layers.Length];
+            for (int i = 0; i < SortingLayer.layers.Length; i++)
+            {
+                layerNames[i] = SortingLayer.layers[i].name;
+            }
+
+            int layerValue = SortingLayer.GetLayerValueFromID(meshRenderer.sortingLayerID);
+            layerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, layerNames);
+
+            SortingLayer layer = SortingLayer.layers[layerValue];
+            meshRenderer.sortingLayerName = layer.name;
+            meshRenderer.sortingLayerID = layer.id;
+            meshRenderer.sortingOrder = EditorGUILayout.IntField("Order in Layer", meshRenderer.sortingOrder);
+        }
+    }
+}
